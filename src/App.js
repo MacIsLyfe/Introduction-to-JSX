28ba8cca8623665bc5cd6a3fc00a2cf1d61261ea
Mac Cooke <101891594+MacIsLyfe@users.noreply.github.com>
2022-10-19 13:09:37 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const shirts = 4;
+const pants = 6;
+const website = "https://www.thinkful.com/";
+
+const element = null;
+
+function App(){
+const element = (
+<main>
+<p className='paragraph'>
+{""}
+Purchased {shirts} shirts and {pants} pants for a total of {shirts + pants} items
+</p>
+<a href = {website}>Website</a>
+</main>
+)
+return element
+}
+
+
+export default App;
